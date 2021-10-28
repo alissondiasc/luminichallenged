be8848ca3a0e39be9c4672cda05d33cf9f980a45
SouthSystem <alisson.cruz@southsystem.com.br>
2021-10-28 14:35:51 -0300

    Inserindo config cors

--- src/main/java/com/lumini/challenge/resource/AddressApi.java
+++ src/main/java/com/lumini/challenge/resource/AddressApi.java
@@ -6,15 +6,13 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
 @RestController
 @RequestMapping(value = "/api/v1/address")
+@CrossOrigin(origins = "http://localhost:4200")
 public interface AddressApi {
 
 
--- src/main/java/com/lumini/challenge/resource/CalculateApi.java
+++ src/main/java/com/lumini/challenge/resource/CalculateApi.java
@@ -5,13 +5,11 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/calculate")
+@CrossOrigin(origins = "http://localhost:4200")
 public interface CalculateApi {
 
     @ApiOperation(value = "Cálculo de médias.",
